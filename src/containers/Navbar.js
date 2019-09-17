8f1077e8c57508df2531f8ac5ce4cf61df2a0039
Aleksandr Vygodchikov <avygodchikov@gmail.com>
2019-09-17 13:46:10 +0300

    second try

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './styles/Navbar.scss'
 
 export const Navbar = () => {
+  let pathname = window.location.pathname
+  console.log(pathname)
+  
   return (
     <div className='navbar'>
         <div className='left-menu'>
@@ -10,10 +13,10 @@ export const Navbar = () => {
         </div>
         <nav>
             <ul>
-              <Link to='/'><li>main</li></Link>
-              <Link to='/stats'><li>stats</li></Link>
-              <Link to='/texts'><li>texts</li></Link>
-              <Link to='/texts'><li>log in</li></Link>
+              <NavLink exact to='/' activeClassName='active-tab'><li>main</li></NavLink>
+              <NavLink to='/stats' activeClassName='active-tab'><li>stats</li></NavLink>
+              <NavLink to='/texts' activeClassName='active-tab'><li>texts</li></NavLink>
+              <NavLink to='/texts' activeClassName='active-tab'><li>log in</li></NavLink>
             </ul>
         </nav>
     </div>
