d4be06609133635eb30e07015468cfc814aa65de
Aleksandr Vygodchikov <avygodhcikov@gmail.com>
2019-09-18 23:36:58 +0300

    6rd

--- src/containers/Texts.js
+++ src/containers/Texts.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import './styles/texts.scss';
 
 export const Texts = () => {
   return (
-    <div className="Texts">
-        
+    <div className="texts">
+        <div className="textsMain">
+          
+        </div>
+        <div className="textsList">
+
+        </div>
     </div>
   );
 }
