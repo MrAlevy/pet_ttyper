691a332cbdf722eca3891b09f9b591951c0f367d
Aleksandr Vygodchikov <avygodhcikov@gmail.com>
2019-09-20 23:29:25 +0300

    work on text

--- src/components/TextsCont/texts-filters.component.jsx
+++ src/components/TextsCont/texts-filters.component.jsx
@@ -8,28 +8,29 @@ const handleClick = (e) => {
 export const TextsFilters = (props) => { 
     return (    
         <div className='texts-filter'>
-            <div                            
-                name='my' 
-                className='filter'
-                onClick={(e) => handleClick(e)} 
-            >
-                My
+            <div className='filters'>
+                <div                            
+                    name='my' 
+                    className='filter f1'
+                    onClick={(e) => handleClick(e)} 
+                >
+                    My
+                </div>
+                <div
+                    name='global' 
+                    className='filter f2' 
+                    onClick={(e) => handleClick(e)}
+                >
+                    Global
+                </div>
+                <div
+                    name='favorites' 
+                    className='filter f3' 
+                    onClick={(e) => handleClick(e)}
+                >
+                    Favorites
+                </div>
             </div>
-            <div
-                name='global' 
-                className='filter' 
-                onClick={(e) => handleClick(e)}
-            >
-                Global
-            </div>
-            <div
-                name='favorites' 
-                className='filter' 
-                onClick={(e) => handleClick(e)}
-            >
-                Favorites
-            </div>
-
             <div className='texts-add-button'>
                 <button onClick={() => props.addTextButtonClick()}>Upload</button>
             </div>
