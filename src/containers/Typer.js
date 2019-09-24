e293a4153113054be61b73a0d92e5658e036b8ee
Aleksandr Vygodchikov <avygodhcikov@gmail.com>
2019-09-25 00:18:45 +0300

    add main typing algorithm

--- src/containers/Typer.js
+++ src/containers/Typer.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { TextItemBodySecondInfo } from '../components/TextsPage/text-item-body-second-info.component'
 import './styles/texts.scss';
+import './styles/typer.scss';
+
 
 export default class Typer extends React.Component {
     constructor (props) {
         super (props)
         this.state = {
-            text: 'test text string of 8 separates, . yes!'.split(' '),
-            inputText: '',
-            inputWordIndex: 0
+            text1: 'test text string of 8 separates, . yes!'.split(' '),
+            inputText1: '',
+            inputWordIndex1: 0,
+
+            rightText: this.props.location.state.body,
+            rightLetter: '',
+            rightTextPast: '',
+            inputLetter: '',
+            inputTextPast: ''
+
           }
           this.handleChange = this.handleChange.bind(this);
         
@@ -31,30 +40,80 @@ export default class Typer extends React.Component {
       }
    
 
+handleInput(e) {
+  const inputValue = e.target.value;
+
+  if (
+    this.state.rightTextPast === '' 
+    && inputValue === ' ' 
+    && this.state.rightLetter === ''
+  ) {
+    // starting space - start the next word, if it is
+    this.setState({
+      rightLetter: this.state.rightText[0],
+      rightText: this.state.rightText.slice(1,),
+    })
+  }
+  else if (inputValue === this.state.rightLetter) {
+    // if input letter is right - send to correct
+    this.setState({
+      rightLetter: this.state.rightText[0],
+      rightText: this.state.rightText.slice(1,),
+      rightTextPast: this.state.rightTextPast + inputValue,
+      inputTextPast: this.state.inputTextPast + inputValue,
+    })
+  }
+  else if (this.state.rightTextPast !== '') {
+    // if input letter is wrong - write the wrong letter to input
+    this.setState({
+      inputLetter: inputValue,
+      inputTextPast: this.state.inputTextPast + inputValue[inputValue.length - 1],
+    })
+  }
+}
+
+
+
     render() {
         return (
             <div className='dataCont typer-cont'>
 
-<div>
-        Вводите следующий текст: {this.state.text.join(' ')} <br/>
-        введено слов: {this.state.inputWordIndex} <br />
-          <input
-            type="text"
-            value={this.state.inputText}
-            name="test"
-            placeholder="test"
-            onChange={this.handleChange}
-          />
-        
-      </div>
+{/*                 <div>
+                  Вводите следующий текст: {this.state.text.join(' ')} <br/>
+                  введено слов: {this.state.inputWordIndex} <br />
+                    <input
+                      type="text"
+                      value={this.state.inputText}
+                      name="test"
+                      placeholder="test"
+                      onChange={this.handleChange}
+                    />
+                  
+                </div> */}
 
 
 
 
+<div id='right-text-line'>
+  <div>{this.state.rightTextPast}</div>
+  <div>{this.state.rightLetter}</div>
+  <div>{this.state.rightText}</div>
+</div>
+<br />
+<div id='input-text-line'>
+  <div>{this.state.inputTextPast}</div>
+  <input                       
+    type="text"
+    value={this.state.inputLetter}
+    onChange={(e) => this.handleInput(e)}
+  />
+</div>
+
+                
 
 
-                {this.props.location.state.body}
-                <input></input>
+
+
                 <TextItemBodySecondInfo 
                     text={this.props.location.state}
                 />
