f2aa5f3bed838f03aa27270c5c68bfdb23fedf55
Aleksandr Vygodchikov <avygodchikov@gmail.com>
2019-10-02 13:39:33 +0300

    final style to speed diagram

--- src/components/TextsPage/typer-main.component.jsx
+++ src/components/TextsPage/typer-main.component.jsx
@@ -8,21 +8,17 @@ export const TyperMain = (props) => {
     } = props.state;
     
     const {
-        caption, rate, owner, bestSpeed, bestSpeedOwnerMistakes, bestSpeedOwner
+        caption, rate, owner, bestSpeed, bestSpeedOwner
     } = props.textInfo;
 
     // calc mistakes persentage
     let miss = Math.round(10* (100*mistakes/lettersEntered) )/10
-//console.log(rightTextPast, rightLetter, rightText)
+
     return (
         <div className='dataCont typer-cont'>
 
             <div id='text-typer-main-info'>
                 <h1>{caption}</h1>
-                <div id='text-typer-best-speed'>
-                    <div>best speed: <div>{bestSpeed} / {bestSpeedOwnerMistakes}%</div></div> 
-                    <div>by <div>{bestSpeedOwner}</div></div>
-                </div>
             </div>
 
             <div>
@@ -34,19 +30,38 @@ export const TyperMain = (props) => {
             </div>
 
             <div id='text-typer-cur-data'>
-                <div>avg speed: <div>{avgSpeed || 0}</div></div>
                 <div>miss: <div>{miss < 100 ? miss : miss >= 100 ? 100 : 0}</div>%</div>
-                curSpeed: {curSpeed}
             </div>
 
             <div id='typer-speed-diagram'>
                 <div>
-                    <div id='cur-speed' style={{width: curSpeed}}>
-                        <div id='avg-speed' style={{width: avgSpeed}}>
-                            <div id='best-speed' style={{width: bestSpeed}}>
-                                
+                    <div id='avg-speed' style={{width: avgSpeed*0.5 || 0}}>
+                        <span id='avg-speed-label'>{avgSpeed}</span>
+                        <div id='cur-speed' style={{width: curSpeed*0.5 || 0}}>
+                            <div id='best-speed' style={{width: bestSpeed*0.5 || 0}}>
+                                <span>
+                                    <span>
+                                        {bestSpeed}
+                                    </span>
+                                    <span>
+                                        <span>by</span>
+                                        {bestSpeedOwner}
+                                    </span>
+                                </span>
                             </div>
                         </div>
+                    </div>
+                    <div 
+                        id='speed-color-background'
+                        style={ 
+                            avgSpeed < bestSpeed 
+                                ? {
+                                    background: 'repeating-linear-gradient(90deg, #61292b00, #85b56c00 1px, #bfb9c200 0px, #6e686f1f 6px) fixed',
+                                } : {
+                                    background: 'repeating-linear-gradient(90deg, rgba(97, 41, 43, 0), rgba(133, 181, 108, 0) 1px, rgba(191, 185, 194, 0) 0px, rgba(8, 148, 31, 0.17) 6px) fixed',
+                                }
+                            }
+                    >
                     </div>
                 </div>
             </div>
@@ -63,7 +78,7 @@ export const TyperMain = (props) => {
 
             <div id='typer-progress'>
                 <div>
-                    <div style={{width: (100*lettersEntered/TEXT_LENGTH) + '%'}}>
+                    <div style={{width: ((100*lettersEntered/TEXT_LENGTH) || 0) + '%'}}>
                     </div>
                 </div>
             </div>
