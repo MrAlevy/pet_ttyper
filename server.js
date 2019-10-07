0633dfc89146b324894b7b812a99f48f0bf47235
Aleksandr Vygodchikov <avygodchikov@gmail.com>
2019-10-07 12:42:27 +0300

    --amend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const config = require('config');
 
 require('dotenv').config();
@@ -22,6 +23,16 @@ connection.once('open', () => {
 
 // use routes
 app.use('/api/texts', require('./routes/texts'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // starting server
 const port = process.env.PORT || 3001;
